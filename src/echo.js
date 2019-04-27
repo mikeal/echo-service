ffc8efba06da87036a8d786575be126fb8484cad
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-04-27 02:33:30 +0000

    fix: no params

--- src/echo.js
+++ src/echo.js
@@ -1,15 +1,10 @@
 const url = require('url')
 const qs = require('querystring')
-
-const cors = res => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-}
+const cors = require('../lib/cors')
 
 module.exports = async (req, res) => {
   let parsed = url.parse(req.url)
-  if (!parsed.query) return req.end()
+  if (!parsed.query) return res.end()
   let params = qs.parse(parsed.query)
   if (params.statusCode) res.statusCode = params.statusCode
   if (params.headers) {
